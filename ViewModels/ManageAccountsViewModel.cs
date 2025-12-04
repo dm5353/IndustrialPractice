d22ca913ecf09b02166c4e69d7e53118ea94d7b8
Nagov <nagovartem@gmail.com>
2025-12-04 03:35:21 +0300

    Управление счетами

---
+++ ViewModels/ManageAccountsViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MyFinance.Models;
+using MyFinance.Services;
+using MyFinance.Views;
+using System.Collections.ObjectModel;
+
+namespace MyFinance.ViewModels
+{
+    public partial class ManageAccountsViewModel : ObservableObject
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        public ObservableCollection<Account> Accounts { get; set; }
+
+        [ObservableProperty]
+        private Account selectedAccount;
+
+        public ManageAccountsViewModel()
+        {
+            Accounts = new ObservableCollection<Account>(_db.Accounts.ToList());
+        }
+
+        [RelayCommand]
+        private void AddAccount()
+        {
+            var win = new AddAccountWindow();
+
+            if (win.ShowDialog() == true)
+            {
+                var newAcc = win.NewAccount;
+
+                _db.Accounts.Add(newAcc);
+                _db.SaveChanges();
+
+                Accounts.Add(newAcc);
+            }
+        }
+
+
+        [RelayCommand]
+        private void DeleteAccount()
+        {
+            if (SelectedAccount == null) return;
+
+            _db.Accounts.Remove(SelectedAccount);
+            _db.SaveChanges();
+
+            Accounts.Remove(SelectedAccount);
+        }
+    }
+}
---
+++ Views/AddAccountWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MyFinance.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MyFinance.Views
+{
+    public partial class AddAccountWindow : Window
+    {
+        public Account NewAccount { get; private set; }
+
+        public AddAccountWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameBox.Text))
+            {
+                MessageBox.Show("Введите название счёта");
+                return;
+            }
+
+            NewAccount = new Account
+            {
+                Name = NameBox.Text.Trim(),
+                Balance = int.Parse(BalanceBox.Text.Trim())
+            };
+
+            DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
