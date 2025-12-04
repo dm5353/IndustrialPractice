922600b4805a785de939baef2fbb51b9db076b25
Nagov <nagovartem@gmail.com>
2025-12-04 03:39:21 +0300

    Revert "Управление счетами"
    This reverts commit 47eaa7ba4ee4efee3ae6d8c3da78ad8730e44a9b.

--- ViewModels/ManageCategoriesViewModel.cs
+++ ViewModels/ManageCategoriesViewModel.cs
@@ -1,9 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using LiveCharts;
+using LiveCharts.Wpf;
+using Microsoft.EntityFrameworkCore;
 using MyFinance.Models;
 using MyFinance.Services;
 using MyFinance.Views;
 using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
 
 namespace MyFinance.ViewModels
 {
